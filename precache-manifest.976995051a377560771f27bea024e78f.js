75318713e519ec08eb5f95369cea1a2f8eb36b68
Zahra <namazi.q@gmail.com>
2022-06-30 13:18:13 +0430

    Updates

---
+++ precache-manifest.976995051a377560771f27bea024e78f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de54d4f63a70257820a36ecc112e1b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d874cc5c57e3211b48e",
+    "url": "./static/css/main.99447fb9.chunk.css"
+  },
+  {
+    "revision": "7f7048e532081567dde4",
+    "url": "./static/js/2.979cc683.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "./static/js/2.979cc683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d874cc5c57e3211b48e",
+    "url": "./static/js/main.81e50f13.chunk.js"
+  },
+  {
+    "revision": "e196c7f473c14ab7633e",
+    "url": "./static/js/runtime-main.3f34e440.js"
+  }
+]);
